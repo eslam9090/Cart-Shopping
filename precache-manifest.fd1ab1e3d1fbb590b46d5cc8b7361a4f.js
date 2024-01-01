881c998829ac03c631754738bc0308d01bf21309
eslammyaakoup <eslammyaakoup7090@gmail.com>
2024-01-01 14:40:28 +0200

    Updates

---
+++ precache-manifest.fd1ab1e3d1fbb590b46d5cc8b7361a4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572c7f5303948d0c56f1379dc48eb500",
+    "url": "/Cart-Shopping/index.html"
+  },
+  {
+    "revision": "cc3565040a46810f1a5c",
+    "url": "/Cart-Shopping/static/css/2.bf6e8f5f.chunk.css"
+  },
+  {
+    "revision": "3c44bb0ea1c77cc39ebf",
+    "url": "/Cart-Shopping/static/css/main.325098ee.chunk.css"
+  },
+  {
+    "revision": "cc3565040a46810f1a5c",
+    "url": "/Cart-Shopping/static/js/2.5f4e5c7d.chunk.js"
+  },
+  {
+    "revision": "ba0745f58ba243534e453c72315f1e5c",
+    "url": "/Cart-Shopping/static/js/2.5f4e5c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0acc0bb1b8041b851d72",
+    "url": "/Cart-Shopping/static/js/3.eccb5c31.chunk.js"
+  },
+  {
+    "revision": "3c44bb0ea1c77cc39ebf",
+    "url": "/Cart-Shopping/static/js/main.84e84a0e.chunk.js"
+  },
+  {
+    "revision": "ac967e9674d2dc3c6d55",
+    "url": "/Cart-Shopping/static/js/runtime-main.086948e8.js"
+  }
+]);
